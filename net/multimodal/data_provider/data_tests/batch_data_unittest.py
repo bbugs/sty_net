030538169fda4166fb01513416c57c1cf22083a9
bbugs <ndo.hoffman@gmail.com>
2016-09-21 14:19:13 +0200

    removed seed

--- net/multimodal/data_provider/data_tests/batch_data_unittest.py
+++ net/multimodal/data_provider/data_tests/batch_data_unittest.py
@@ -17,9 +17,9 @@ batch_data = BatchData(json_fname, cnn_fname, imgid2region_indices, w2v_vocab_fn
 X_img, X_txt, region2pair_id, word2pair_id = batch_data.get_minibatch(batch_size=5, verbose=True)
 
 
-X_img2, X_txt2, region2pair_id2, word2pair_id2 = batch_data.get_minibatch(batch_size=5, verbose=True, seed=None)
+X_img2, X_txt2, region2pair_id2, word2pair_id2 = batch_data.get_minibatch(batch_size=5, verbose=True)
 
-X_img3, X_txt3, region2pair_id3, word2pair_id3 = batch_data.get_minibatch(batch_size=5, verbose=True, seed=None)
+X_img3, X_txt3, region2pair_id3, word2pair_id3 = batch_data.get_minibatch(batch_size=5, verbose=True)
 
-X_img4, X_txt4, region2pair_id4, word2pair_id4 = batch_data.get_minibatch(batch_size=5, verbose=True, seed=None)
+X_img4, X_txt4, region2pair_id4, word2pair_id4 = batch_data.get_minibatch(batch_size=5, verbose=True)
 # TODO: extend tests with asserts
