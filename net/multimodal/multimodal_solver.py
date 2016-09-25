9bc36307b7d89f7ffd6e70783b9ef886114165a2
bbugs <ndo.hoffman@gmail.com>
2016-09-25 12:29:38 +0200

    added todo

--- net/multimodal/multimodal_solver.py
+++ net/multimodal/multimodal_solver.py
@@ -163,7 +163,7 @@ class MultiModalSolver(object):
         # Perform a parameter update
         for p, w in self.model.params.iteritems():
             dw = grads[p]
-            if dw is None:  # when usefinetune is False, some gradients will be None
+            if dw is None:  # when usefinetune is False, some gradients will be None #todo: check if finetuning is done properly
                 continue
             config = self.optim_configs[p]
             next_w, next_config = self.update_rule(w, dw, config)
